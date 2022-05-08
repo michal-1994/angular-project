2c033321fcef4d94cdc5e4f61dda126fc1409a88
Michal <michalgrzegorczyk04@gmail.com>
2022-05-08 10:41:40 +0200

    Add ingredients to recipes

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,12 +1,31 @@
 import { EventEmitter } from "@angular/core";
+import { Ingredient } from "../shared/ingredient.model";
 import { Recipe } from "./recipe.model";
 
 export class RecipeService {
     recipeSelected = new EventEmitter<Recipe>();
 
     private recipes: Recipe[] = [
-        new Recipe('A test Recipe 1', 'This is simply a test', 'https://images.pexels.com/photos/2097090/pexels-photo-2097090.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260'),
-        new Recipe('A test Recipe 2', 'This is simply a test', 'https://images.pexels.com/photos/2097090/pexels-photo-2097090.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260')
+        new Recipe(
+            'Tasty Schnitzel',
+            'A super tasty Schnitzel - just awesome!',
+            'https://upload.wikimedia.org/wikipedia/commons/thumb/2/22/Breitenlesau_Krug_Br%C3%A4u_Schnitzel.JPG/800px-Breitenlesau_Krug_Br%C3%A4u_Schnitzel.JPG',
+            [
+                new Ingredient('Meat', 1),
+                new Ingredient('French Fries', 20)
+            ]
+        ),
+        new Recipe(
+            'Big Fat Burger',
+            'What else you need to say?',
+            'https://upload.wikimedia.org/wikipedia/commons/thumb/6/62/NCI_Visuals_Food_Hamburger.jpg/800px-NCI_Visuals_Food_Hamburger.jpg',
+            [
+                new Ingredient('Buns', 2),
+                new Ingredient('Meat', 1),
+                new Ingredient('French Fries', 20)
+            ]
+        )
+
     ];
 
     getRecipes() {
