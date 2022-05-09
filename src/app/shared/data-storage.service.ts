70421261ab5a6acd8102434abc6f49f2a9c371b2
Michal <michalgrzegorczyk04@gmail.com>
2022-05-09 12:19:46 +0200

    Store recipes

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,9 +1,20 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import { RecipeService } from "../recipes/recipe.service";
 
 @Injectable({providedIn: 'root'})
 export class DataStorageService {
     constructor(
-        private http: HttpClient
+        private http: HttpClient,
+        private recipeService: RecipeService
     ) {}
+
+    storeRecipes() {
+        const recipes = this.recipeService.getRecipes();
+        this.http.put('https://ng-course-recipe-book-e1d2d-default-rtdb.firebaseio.com/recipes.json', recipes).subscribe(
+            (respose) => {
+                console.log(respose);
+            }
+        );
+    }
 }
